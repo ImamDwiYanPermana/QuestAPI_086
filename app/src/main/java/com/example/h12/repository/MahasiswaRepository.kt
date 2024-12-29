15ff136f3a46c860dc064fc0cadf6eb169a758b5
ImamDwiYanPermana <imampermana244@gmail.com>
2024-12-30 05:23:32 +0700

    membuat class dan memanggil MahasiswaRepository dan repository dan mendeklarasi fungsi

--- app/src/main/java/com/example/h12/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/h12/repository/MahasiswaRepository.kt
@@ -1,6 +1,8 @@
 package com.example.h12.repository
 
 import com.example.h12.data.Mahasiswa
+import com.example.h12.service.MahasiswaService
+import okio.IOException
 
 
 interface MahasiswaRepository{
@@ -14,4 +16,38 @@ interface MahasiswaRepository{
     suspend fun deleteMahasiswa(nim: String)
 
     suspend fun getMahasiswaById(nim: String): Mahasiswa
+}
+class NetworkMahasiswaRepository(
+
+    private val mahasiswaService: MahasiswaService
+): MahasiswaRepository{
+
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaService.deleteMahasiswa(nim)
+            if (!response.isSuccessful){
+                throw IOException("Failed to delete mahasiswa. HTTP Status code:" +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaService.getMahasiswa()
+
+    override suspend fun getMahasiswaById(nim: String): Mahasiswa {
+        return mahasiswaService.getMahasiswaById(nim)
+    }
 }
