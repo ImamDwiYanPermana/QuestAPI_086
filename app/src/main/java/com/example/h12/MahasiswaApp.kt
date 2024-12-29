2c0e0be48233fdca50250cb8a6aa72924264bdbc
ImamDwiYanPermana <imampermana244@gmail.com>
2024-12-30 05:53:56 +0700

    membuat class MahasiswaApp dari yang di ambil dari Application

--- app/src/main/java/com/example/h12/MahasiswaApp.kt
+++ app/src/main/java/com/example/h12/MahasiswaApp.kt
@@ -1,2 +1,14 @@
 package com.example.h12
 
+import android.app.Application
+import com.example.h12.dependenciesinjection.AppContainer
+import com.example.h12.dependenciesinjection.MahasiswaContainer
+
+class MahasiswaApp: Application() {
+
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = MahasiswaContainer()
+    }
+}
